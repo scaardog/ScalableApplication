c5ac34c8aceba84b4b5ff9c91005985d24209f03
ANechaev <ANechaev@sportmaster.ru>
2021-10-31 16:27:05 +0300

    модуль для стороннего сервиса

---
+++ externalservice/src/main/java/com/example/externalservice/ExternalserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.externalservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExternalserviceApplication.class, args);
+    }
+
+}
