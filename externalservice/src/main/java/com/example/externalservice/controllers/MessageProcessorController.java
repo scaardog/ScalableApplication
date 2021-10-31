fef4bb9fceabff09ec02d47b15e397a7f30d8c49
ANechaev <ANechaev@sportmaster.ru>
2021-10-31 17:14:38 +0300

    Внешний сервис и обращение к нему из клиентсокого приложения

--- externalservice/src/main/java/com/example/externalservice/controllers/MessageProcessorController.java
+++ externalservice/src/main/java/com/example/externalservice/controllers/MessageProcessorController.java
@@ -2,13 +2,14 @@ package com.example.externalservice.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageProcessorController {
 
     @GetMapping("/doSomething")
-    public ResponseEntity<String> doSomething(String message) {
+    public ResponseEntity<String> doSomething(@RequestParam("message") String message) {
         StringBuilder sb = new StringBuilder(message);
         sb.reverse();
         return ResponseEntity.ok(sb.toString());
