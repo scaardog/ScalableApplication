8e3f5e15777e642f0f44020c725b29e60f9332a9
ANechaev <ANechaev@sportmaster.ru>
2021-11-02 18:48:17 +0300

    Ограничение лимитов запросов для внешнего сервиса

--- externalservice/src/main/java/com/example/externalservice/controllers/MessageProcessorController.java
+++ externalservice/src/main/java/com/example/externalservice/controllers/MessageProcessorController.java
@@ -1,17 +1,34 @@
 package com.example.externalservice.controllers;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Bucket4j;
+import io.github.bucket4j.Refill;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+
 @RestController
 public class MessageProcessorController {
+    private final Bucket bucket;
+
+    public MessageProcessorController() {
+        this.bucket = Bucket4j.builder()
+                .addLimit(Bandwidth.classic(2, Refill.intervally(10, Duration.ofSeconds(10))))
+                .build();
+    }
 
     @GetMapping("/doSomething")
     public ResponseEntity<String> doSomething(@RequestParam("message") String message) {
-        StringBuilder sb = new StringBuilder(message);
-        sb.reverse();
-        return ResponseEntity.ok(sb.toString());
+        if (bucket.tryConsume(1)) {
+            StringBuilder sb = new StringBuilder(message);
+            sb.reverse();
+            return ResponseEntity.ok(sb.toString());
+        }
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 }
